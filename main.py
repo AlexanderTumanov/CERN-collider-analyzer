c048d37b0515c8b1df9af8aea259bafb02b0d0ae
Alexander Tumanov <alexander.tumanov.jr@gmail.com>
2025-08-16 11:59:39 -0400

    generalized

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import argparse
 
+# Semi-agressive basic IQR filter
 def iqr_filter(data, col):
     Q1 = data[col].quantile(0.25)
     Q3 = data[col].quantile(0.75)
@@ -27,24 +28,30 @@ def plot_graph(df, graph, x_column, y_column=None, title_suffix=""):
         plt.xlabel(f'{x_column} ({units.get(x_column, "")})')
         plt.ylabel(f'{y_column} ({units.get(y_column, "")})')
 
-components = ['px1', 'py1', 'pz1', 'px2', 'py2', 'pz2']
+components = ['px1', 'py1', 'pz1', 'px2', 'py2', 'pz2'] # Momentum components
 units = {
-    'E1': 'GeV',
+    'E1': 'GeV', # Energies
     'E2': 'GeV',
-    'pt1': 'GeV',
+    'pt1': 'GeV', # Transverse momentum
     'pt2': 'GeV',
-    'phi1': 'rad',
+    'phi1': 'rad', # Phi angle
     'phi2': 'rad',
-    'M': 'GeV'
+    'M': 'GeV' # Invariant Mass
 }
 units.update({key: 'GeV/c' for key in components})
 
 def main():
-    df = pd.read_csv('dielectron.csv')
+    df = pd.read_csv('dielectron.csv') # Input csv file to visualize
 
+    # fill null values with mean
+    df.fillna(df.mean(), inplace=True)
+    
+    # remove duplicate rows
+    df.drop_duplicates(inplace=True)
+    
     parser = argparse.ArgumentParser(
-        prog='CERN Data Visualization',
-        description='Visualize CERN electron collision data'
+        prog='Data Visualization',
+        description='Visualize data :0'
     )
     parser.add_argument('Graph_Type', choices=['histogram', 'scatter', 'line'], help='Type of graph to plot')
     parser.add_argument('x_column', help='Name of the x column to plot')
@@ -62,7 +69,7 @@ def main():
     if y_column:
         filtered_df = iqr_filter(filtered_df, y_column)
     plot_graph(filtered_df, graph, x_column, y_column, title_suffix=" (IQR Filtered)")
-
+    
     plt.show()
 
 if __name__ == "__main__":
